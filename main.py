8444997a4c301bb2738184504613761b97f79534
rheamaetorres <ryanrafaelraymundo@gmail.com>
2023-07-25 06:21:48 +0800

    python class

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print ('hello world')
+class Item:
+    def __init__(self,name) -> None:
+        print(f"an instance created:{name}")
+    def calculate_total_price(self,x,y):
+        return x * y
+
+
+
+item1 = Item("Phone")
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
+
+
+
+item2 = Item("Laptop")
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
