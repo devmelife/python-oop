1e59f60d77acab8a33e773749a0dd6a59f8b1e32
rheamaetorres <ryanrafaelraymundo@gmail.com>
2023-07-29 08:17:16 +0800

    class __init__

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 class Item:
-    def __init__(self,name) -> None:
-        print(f"an instance created:{name}")
-    def calculate_total_price(self,x,y):
-        return x * y
+    def __init__(self,name:str,price:float,quantity = 0):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    def __str__(self) :
+        return  f"the item is {self.name} the price is {self.price} the quantity is {self.quantity}"
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 
-item1 = Item("Phone")
-item1.name = "Phone"
-item1.price = 100
-item1.quantity = 5
+item1 = Item("Phone", 500.2,2)
+item2 = Item("Laptop", 1000, 2)
 
-
-
-item2 = Item("Laptop")
-item2.name = "Laptop"
-item2.price = 1000
-item2.quantity = 3
+print(item1.calculate_total_price())
